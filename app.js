7d02454f2699bd8353530020a23868790d389287
DanteMiron <dantemiron@hotmail.com>
2022-04-25 22:25:23 -0300

    primera tres vistas

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const publicPath = path.resolve(__dirname, './public');
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => console.log("Servidor Funcionando"));
+app.listen(3001, () => console.log("Servidor Funcionando"));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
